ebd2cb252ee6e59c180c30e21e230e2903846660
Ben Chang <ben.chang@intel.com>
2021-07-29 17:17:52 +0800

    Add files via upload

--- guessnumber.py
+++ guessnumber.py
@@ -1,13 +1,21 @@
 import random
-ans = random.randint(1, 100)
 
+start = input('pleaes choose the maxmum number: ')
+start = int(start)
+end = input('please choose the minmum number: ')
+end = int(end)
+ans = random.randint(end, start)
+count = 0
 while True:
-    guess = input('Please guess the number from 1 to 100: ')
+    print('the number start from', end, 'to', start)
+    guess = input('Please guess the number: ')
     guess = int(guess)
+    count += 1 #count = count + 1
     if ans > guess:
        print('make it bigger')
     elif ans < guess:
        print('make it smaller') 
     else:
-       print('You got it')        
+       print('You got it in', count, 'times')   
+
        break
